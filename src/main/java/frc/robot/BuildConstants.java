d4c7b05e1105cacefbfd6676c72839044aa0ee16
noahbedouch <noahbedouch@gmail.com>
2025-04-29 20:56:56 -0700

    Add Vision Measurements to Pose Estimation

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC-2025-Rework";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 1;
-  public static final String GIT_SHA = "a1894533d1c2c37a668a2d267e7f232f953420c9";
-  public static final String GIT_DATE = "2025-04-09 01:29:02 PDT";
+  public static final int GIT_REVISION = -1;
+  public static final String GIT_SHA = "1b61aae8bbc5dd4a5fd2960221a0709246874979";
+  public static final String GIT_DATE = "2025-04-24 16:44:42 PDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-04-24 16:38:52 PDT";
-  public static final long BUILD_UNIX_TIME = 1745537932047L;
+  public static final String BUILD_DATE = "2025-04-29 19:34:53 PDT";
+  public static final long BUILD_UNIX_TIME = 1745980493975L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
